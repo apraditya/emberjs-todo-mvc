69a6f94c076cc2e2d2226728cb90f59cd90e66b6
Adinda Praditya <apraditya@gmail.com>
2015-11-20 16:43:41 +0800

    Use Ember Component instead of controller

--- app/components/todos-container.js
+++ app/components/todos-container.js
@@ -1,13 +1,14 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
   actions: {
     createTodo: function () {
       var title = this.get('new_title');
 
       if (!title.trim()) { return; }
 
-      var todo  = this.store.createRecord('todo', {
+      var todo  = this.get('store').createRecord('todo', {
         title: title,
         isCompleted: false
       });
---
+++ tests/integration/components/todos-container-test.js
@@ -0,0 +1,37 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('todos-container', 'Integration | Component | todos container', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(1);
+
+  this.render(hbs`{{todos-container}}`);
+
+  assert.equal(this.$().text().trim(), 'Todos');
+});
+
+test('it renders with existing todos', function(assert) {
+  var todoModels  = [
+    { id: 1,
+      title: 'buy CSS book',
+      'isCompleted': false
+    },
+    { id: 2,
+      title: 'learn CSS',
+      'isCompleted': false
+    },
+    { id: 3,
+      title: 'rest',
+      'isCompleted': true
+    },
+  ];
+
+  this.set('todo-models', todoModels);
+  this.render(hbs`{{todos-container todos=todo-models}}`);
+
+  assert.equal(this.$('#todo-list li').length, 3);
+  assert.notOk(this.$('#todo-list li:first').hasClass('completed'));
+});
